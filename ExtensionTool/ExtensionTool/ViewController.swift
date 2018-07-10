9da0984dfbdd166093100a5a0b16ff6b096fdfa3
ZCC <18311296151@163.com>
2018-07-10 10:22:32 +0800

    删除垃圾代码

--- ExtensionTool/ExtensionTool/ViewController.swift
+++ ExtensionTool/ExtensionTool/ViewController.swift
@@ -13,26 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = UIColor.k_colorWith(r: 216.0, g: 216.0, b: 216.0)
-        
-        self.view.addSubview(self.imgV)
-        
-        self.imgV.addTarget(self, action: #selector(imgAction))
-    }
-
-    @objc func imgAction() {
-        
-        
+   
     }
     
-    lazy var imgV: CircleImageView = {
-        let imgV = CircleImageView.init(frame: CGRect(x: 0.0, y: 0.0, width: 100.0, height: 100.0))
-        imgV.center = CGPoint.init(x: self.view.center.x, y: self.view.center.y - 120.0)
-        imgV.image = UIImage.init(named: "1")
-//        imgV.image = UIImage.init(named: "1")?.k_circleImage(backColor: self.view.backgroundColor!)
-
-        return imgV
-    }()
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
